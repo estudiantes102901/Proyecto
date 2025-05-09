1e76cb98fc67b3813473c167dd689fbc2f6b1493
estudiantes102901 <sahamarymaterano@gmail.com>
2025-05-09 09:13:00 -0400

    new file:   api-rest/app/Exceptions/Handler.php 	modified:   api-rest/app/Http/Controllers/AuthController.php 	modified:   api-rest/app/Http/Controllers/Controller.php 	modified:   api-rest/app/Http/Controllers/ProductController.php 	modified:   api-rest/app/Models/Product.php 	modified:   api-rest/routes/api.php

--- api-rest/app/Http/Controllers/ProductController.php
+++ api-rest/app/Http/Controllers/ProductController.php
@@ -31,7 +31,15 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'amount' => 'required|numeric|min:0',
+        ]);
+
+        $product = Product::create($validatedData);
+
+        return response()->json($product, 201);
     }
 
     /**
@@ -39,7 +47,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return response()->json($product);
     }
 
     /**
@@ -55,7 +63,15 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'sometimes|required|string',
+            'amount' => 'sometimes|required|numeric|min:0',
+        ]);
+
+        $product->update($validatedData);
+
+        return response()->json($product);
     }
 
     /**
@@ -63,6 +79,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json(null, 204); // 204 No Content
     }
 }
--- api-rest/routes/api.php
+++ api-rest/routes/api.php
@@ -12,9 +12,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
+Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('reset-password', [AuthController::class, 'resetPassword']);
+
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::get('logout', [AuthController::class, 'logout']);
-
     Route::get('products', [ProductController::class, 'index']);
+    Route::post('products', [ProductController::class, 'store']);
+    Route::get('products/{product}', [ProductController::class, 'show']);
+    Route::put('products/{product}', [ProductController::class, 'update']);
+    Route::delete('products/{product}', [ProductController::class, 'destroy']);
 });
